11434ecbcdb288a8ae93485b07a815a2f528df7b
ankurpowerplay <ankur.jar123@gmail.com>
2023-04-09 13:20:52 +0530

    add: scss setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
-import '@/styles/globals.css'
+// import '../styles/globals.css'
+import styles from "./../styles/all.scss"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
