432635ed33d63f3a0f09012bfb3160809e8faa95
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2023-04-10 01:34:50 +0530

    Added Responsiveness to the Dashboard

--- component/NavBar.js
+++ component/NavBar.js
@@ -2,7 +2,21 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import {useProSidebar} from 'react-pro-sidebar'
 const Navbar = () => {
-    const { collapseSidebar } = useProSidebar();
+    const { collapseSidebar,broken } = useProSidebar();
+    if(broken)
+    return(
+        <nav class="customNavbar sticky-top">
+        <div className="brandLogo d-flex justify-content-between">
+            <img src="/o1.png" height="40"/>
+            <div className="toggleButton mt-2" onClick={() => collapseSidebar()}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+            </div>
+        </div>
+
+        </nav>
+    )
+    else
     return ( 
         <nav class="customNavbar sticky-top">
         <div className="brandLogo d-flex">
